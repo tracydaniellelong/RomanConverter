de90b0eb015d9ca5de9e27ffefd81b631f79c1d5
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-15 15:18:26 -0400

    1 = I test passed

--- romanconverter.rb
+++ romanconverter.rb
@@ -1,9 +1,8 @@
-def roman(num)
-	if num == 1
-		return "I"
-	elsif num == 5
-		return "V"
-	else
-		return num
-	end
+def roman(number)
+	values = {1 => 'I'}
+	number = values[0] 
+	letter = values[1]
+	return letter
 end
+	
+
