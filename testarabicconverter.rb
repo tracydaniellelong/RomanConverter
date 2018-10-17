c6d7616a758deab4eead936a25fa88fafb4167c2
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-17 13:49:23 -0400

    more testing

--- testarabicconverter.rb
+++ testarabicconverter.rb
@@ -20,17 +20,39 @@ class Testing < Minitest::Test
 	def test_10
 		assert_equal(10, "X".convert())
 	end
+	def test_40
+		assert_equal(40, "XL".convert())
+	end
+	def test_49
+		assert_equal(49, "XLIX".convert())
+	end
 	def test_50
 		assert_equal(50, "L".convert())
+	end
+	def test_90
+		assert_equal(90, "XC".convert())
+	end
+	def test_99
+		assert_equal(99, "XCIX".convert())
 	end
 	def test_100
 		assert_equal(100, "C".convert())
 	end
+	def test_400
+		assert_equal(400, "CD".convert())
+	end
+	def test_499
+		assert_equal(499, "CDXCIX".convert())
+	end
 	def test_500
 		assert_equal(500, "D".convert())
+	end
+	def test_900
+		assert_equal(900, "CM".convert())
 	end
 	def test_1000
 		assert_equal(1000, "M".convert())
 	end
+
 
 end
