98f037cc9e650e74429647fc34e11b44ca1fddcb
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-17 13:30:43 -0400

    working for numbers up to 8

--- arabicconverter.rb
+++ arabicconverter.rb
@@ -3,15 +3,20 @@ class String
 	def convert()
 
 		numbers = {
+			"IV" => 4,
+			"V" => 5,
 			"I" => 1
 		}
 
-		arabic = ""
-		num = self
-		numbers.each do |key, letter|
-			arabic << key
+		arabic = 0
+		numeral = self
+
+		numbers.each do |letter, value|
+			arabic += value * (numeral.scan(/#{letter}/).count)
+			temp = [letter]
+			temp.each {|v| numeral.sub!(v, '')}
 		end
+
+		return arabic
 	end
-
-
 end
--- testarabicconverter.rb
+++ testarabicconverter.rb
@@ -2,9 +2,23 @@ require 'minitest/autorun'
 require_relative 'arabicconverter.rb'
 
 class Testing < Minitest::Test
-	def test_integer
-		assert_equals(Integer, 1.convert().class)
+	def test_string
+		assert_equal(Integer, "I".convert().class)
 	end
-
+	def test_1
+		assert_equal(1, "I".convert())
+	end
+	def test_4
+		assert_equal(4, "IV".convert())
+	end
+	def test_5
+		assert_equal(5, "V".convert())
+	end
+	def test_9
+		assert_equal(9, "IV".convert())
+	end
+	def test_10
+		assert_equal(10, "X".convert())
+	end
 
 end
